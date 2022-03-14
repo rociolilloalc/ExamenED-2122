96c640df6677ad5b47165f8f9930cc29e246069c
jrgs <jrgarcia@iesmarenostrum.com>
2022-03-14 10:01:31 +0100

    Versión inicial del proyecto para el examen de Entornos de desarrollo, curso 2021-22

---
+++ ExamenED2122/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ExamenED2122
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtImporte = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbNormal = new System.Windows.Forms.RadioButton();
+            this.rbReducido = new System.Windows.Forms.RadioButton();
+            this.rbSuper = new System.Windows.Forms.RadioButton();
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btTotal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtImporte
+            // 
+            this.txtImporte.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImporte.Location = new System.Drawing.Point(174, 32);
+            this.txtImporte.Name = "txtImporte";
+            this.txtImporte.Size = new System.Drawing.Size(339, 45);
+            this.txtImporte.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Importe:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tipo de IVA:";
+            // 
+            // rbNormal
+            // 
+            this.rbNormal.AutoSize = true;
+            this.rbNormal.Checked = true;
+            this.rbNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbNormal.ForeColor = System.Drawing.Color.Blue;
+            this.rbNormal.Location = new System.Drawing.Point(281, 124);
+            this.rbNormal.Name = "rbNormal";
+            this.rbNormal.Size = new System.Drawing.Size(143, 42);
+            this.rbNormal.TabIndex = 3;
+            this.rbNormal.TabStop = true;
+            this.rbNormal.Text = "Normal";
+            this.rbNormal.UseVisualStyleBackColor = true;
+            // 
+            // rbReducido
+            // 
+            this.rbReducido.AutoSize = true;
+            this.rbReducido.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbReducido.ForeColor = System.Drawing.Color.Blue;
+            this.rbReducido.Location = new System.Drawing.Point(281, 173);
+            this.rbReducido.Name = "rbReducido";
+            this.rbReducido.Size = new System.Drawing.Size(176, 42);
+            this.rbReducido.TabIndex = 4;
+            this.rbReducido.Text = "Reducido";
+            this.rbReducido.UseVisualStyleBackColor = true;
+            // 
+            // rbSuper
+            // 
+            this.rbSuper.AutoSize = true;
+            this.rbSuper.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSuper.ForeColor = System.Drawing.Color.Blue;
+            this.rbSuper.Location = new System.Drawing.Point(281, 222);
+            this.rbSuper.Name = "rbSuper";
+            this.rbSuper.Size = new System.Drawing.Size(259, 42);
+            this.rbSuper.TabIndex = 5;
+            this.rbSuper.Text = "Súper reducido";
+            this.rbSuper.UseVisualStyleBackColor = true;
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCalcular.Location = new System.Drawing.Point(544, 27);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(170, 61);
+            this.btCalcular.TabIndex = 6;
+            this.btCalcular.Text = "Añadir";
+            this.btCalcular.UseVisualStyleBackColor = true;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(174, 338);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(339, 45);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // btTotal
+            // 
+            this.btTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTotal.Location = new System.Drawing.Point(544, 338);
+            this.btTotal.Name = "btTotal";
+            this.btTotal.Size = new System.Drawing.Size(170, 56);
+            this.btTotal.TabIndex = 8;
+            this.btTotal.Text = "Total";
+            this.btTotal.UseVisualStyleBackColor = true;
+            this.btTotal.Click += new System.EventHandler(this.btTotal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btTotal);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btCalcular);
+            this.Controls.Add(this.rbSuper);
+            this.Controls.Add(this.rbReducido);
+            this.Controls.Add(this.rbNormal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtImporte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtImporte;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbNormal;
+        private System.Windows.Forms.RadioButton rbReducido;
+        private System.Windows.Forms.RadioButton rbSuper;
+        private System.Windows.Forms.Button btCalcular;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btTotal;
+    }
+}
+
---
+++ ExamenED2122/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExamenED2122
+{
+    public partial class Form1 : Form
+    {
+        double cantidadTotal;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            double importe = Convert.ToDouble(txtImporte.Text);
+            double IVA = 0.21;
+            if (rbReducido.Checked == true)  // IVA reducido, 10%
+                IVA = 0.1;
+            if (rbSuper.Checked == true)  // IVA super reducido, 4%
+                IVA = 0.4;
+            importe = importe * IVA; // Calculamos el importe con IVA
+            cantidadTotal = importe; // Se lo añadimos a la cantidad total
+            txtResultado.Text = Convert.ToString(importe);
+        }
+
+        private void btTotal_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = Convert.ToString(cantidadTotal);
+        }
+    }
+}
